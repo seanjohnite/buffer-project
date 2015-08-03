5d80bc2706edaa44771d3ff4de510b538453d80c
Justin Barabander <justinbarabander@Justins-MacBook-Pro.local>
2015-08-03 09:11:14 -0400

    Started Work on Home page and finished csv parser

---
+++ js/atomicWeightParser.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+
+function atomicParse(){
+  var atomicTable = [];
+  fs.readFile('pt-data1.csv',function(err, data) {
+    if(err){
+      throw new Error(err);
+    }
+    var lines =  data.toString().split('\n');
+    var titles = lines[0];
+    lines = lines.slice(1);
+    for (var i = 0; i < lines.length; i++) {
+      var x = lines[i].split(',');
+      obj = {};
+      obj[titles[3]] = Math.round(Number(x[3].replace(/\(\d+\)$/, '')) * 100) / 100;
+      obj[titles[2]] = x[2];
+      obj[titles[1]] = x[1];
+      obj[titles[0]] = x[0];
+      atomicTable.push(obj);
+    }
+  });
+  return atomicTable;
+}
+
+module.exports = atomicParse;
